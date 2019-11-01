512551387ce4e4b9b3704a2d62ae07e3f8db81d9
Bora Manga <bora.mng525@gmail.com>
2019-11-01 12:17:34 +0100

    gitignore file changes

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("12345-67890-09876-54321"));
-//AUTHENTICATION
+//AUTHENTICATION/COOKIES
 function auth(req, res, next) {
   if (!req.signedCookies.user) {
     var authHeader = req.headers.authorization;
